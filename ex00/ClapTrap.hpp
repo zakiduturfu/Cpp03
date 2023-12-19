9fd386a3137fcf2d35f0cfebb2f5434a36fa4da7
Zacharie LEMERY <zlemery@made-f0Ar9s1.clusters.42paris.fr>
2023-12-19 18:58:51 +0100

    ex00 fini

---
+++ ex00/ClapTrap.hpp
@@ -0,0 +1,48 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ClapTrap.hpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: zlemery <zlemery@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/12/19 13:33:29 by zlemery           #+#    #+#             */
+/*   Updated: 2023/12/19 16:22:45 by zlemery          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+
+#ifndef CLAPTRAP_HPP
+#define CLAPTRAP_HPP
+
+#define RESET   "\033[0m"
+#define BLACK   "\033[30m"      /* Noir */
+#define RED     "\033[31m"      /* Rouge */
+#define GREEN   "\033[32m"      /* Vert */
+#define YELLOW  "\033[33m"      /* Jaune */
+#define BLUE    "\033[34m"      /* Bleu */
+
+
+#include <iostream>
+
+class ClapTrap
+{
+public:
+	ClapTrap( const std::string name );
+	ClapTrap( ClapTrap const & src);
+	~ClapTrap();
+
+	ClapTrap	& operator=( ClapTrap const & rhs );
+
+	void	attack( const std::string& target );
+	void	takeDamage( unsigned int amount );
+	void	beRepaired( unsigned int amount );
+
+private:
+
+	const std::string	_Name;
+	int			_HitPoints;
+	int			_EnergyPoints;
+	int			_AttackDamage;	
+};
+
+#endif
